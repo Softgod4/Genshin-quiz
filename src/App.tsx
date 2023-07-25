dcbc98dd485d3613e2c59d61a89c98a226036e0d
Softgodworld <softgod2005@gmail.com>
2023-07-25 17:01:14 +0300

    залил полуготовый

--- src/App.tsx
+++ src/App.tsx
@@ -25,22 +25,23 @@ function Quiz() {
   useEffect(() => {
     let random = Math.floor(Math.random() * 10); 
     setScore(score + random)
-    console.log(score)
   }, [count])
+
+  let img = `/genshin-card/${count + 1}.png`
 
   return (
     <div className="main flex flex-col justify-center items-center h-screen w-screen">
-      <div className="window flex flex-col justify-center items-center p-4 bg-white rounded-xl max-w-2xl">
+      <div className="window flex flex-col justify-center items-center p-4 bg-white rounded-xl max-w-2xl max-h-70%">
         
-        <img src="/genshin-logo.png" alt="" className='w-2/3 rounded-2xl mb-2'/>
-        <h1 className='h1_title text-black text-center'>{quizData[count].question}</h1>
-        <p className='mb-12 text-xl'>{count} из 50</p>
+        <img src={img} alt="" className='max-w-full max-h-[50%] rounded-2xl mb-2'/>
+        <h1 className='h2_title text-black text-center'>{quizData[count].question}</h1>
+        <p className='mb-12 text-xl'>{count} из 42</p>
 
         <div className="grid gap-4 grid-cols-2 grid-rows-2">
-          <button onClick={() => setCount(count + 1)} className='glow-on-hover text-xl bg-cyan-950 text-white py-2 px-20 rounded-xl border-solid'>{quizData[count].answers[0].value}</button>
-          <button onClick={() => setCount(count + 1)} className='glow-on-hover text-xl bg-cyan-950 text-white py-2 px-20 rounded-xl border-solid'>{quizData[count].answers[0].value}</button>
-          <button onClick={() => setCount(count + 1)} className='glow-on-hover text-xl bg-cyan-950 text-white py-2 px-20 rounded-xl border-solid'>{quizData[count].answers[0].value}</button>
-          <button onClick={() => setCount(count + 1)} className='glow-on-hover text-xl bg-cyan-950 text-white py-2 px-20 rounded-xl border-solid'>{quizData[count].answers[0].value}</button>
+          <button onClick={() => setCount(count + 1)} className='glow-on-hover text-xl bg-cyan-950 text-white py-2 px-20 rounded-xl border-solid max-w-3xl max-h-12 text-center'>{quizData[count].answers[0].value}</button>
+          <button onClick={() => setCount(count + 1)} className='glow-on-hover text-xl bg-cyan-950 text-white py-2 px-20 rounded-xl border-solid max-w-3xl max-h-12 text-center'>{quizData[count].answers[1].value}</button>
+          <button onClick={() => setCount(count + 1)} className='glow-on-hover text-xl bg-cyan-950 text-white py-2 px-20 rounded-xl border-solid max-w-3xl max-h-12 text-center'>{quizData[count].answers[2].value}</button>
+          <button onClick={() => setCount(count + 1)} className='glow-on-hover text-xl bg-cyan-950 text-white py-2 px-20 rounded-xl border-solid max-w-3xl max-h-12 text-center'>{quizData[count].answers[3].value}</button>
         </div>
 
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      maxHeight: {
+        '70%': '526px'
+      }
+    },
   },
   plugins: [],
 };
